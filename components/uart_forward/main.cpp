58654b5ba063355d44f07487551104255267b787
FancyOtter99 <pizza.great@protonmail.com>
2025-06-12 10:49:07 -0400

    Update main.cpp

--- components/uart_forward/main.cpp
+++ components/uart_forward/main.cpp
@@ -1,6 +1,10 @@
+#include "esphome.h"
 #include "uart_forward.h"
 
+using namespace esphome;
+
 void setup() {
-  auto my_uart = new esphome::uart_forward::UARTForwardComponent(id(my_uart));
+  auto *my_uart = new uart_forward::UARTForwardComponent(get_uart_by_id("my_uart"));
   App.register_component(my_uart);
 }
+
