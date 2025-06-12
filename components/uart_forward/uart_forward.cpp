7e67476efe8560ac85f16f2e8a3dd6b7e43375fb
FancyOtter99 <pizza.great@protonmail.com>
2025-06-12 10:31:46 -0400

    Update uart_forward.cpp

--- components/uart_forward/uart_forward.cpp
+++ components/uart_forward/uart_forward.cpp
@@ -1,5 +1,17 @@
+// uart_forward.cpp
 #include "uart_forward.h"
 
-// Nothing needed here since everything is inline in .h except includes
+UARTForwardComponent::UARTForwardComponent(esphome::UARTComponent *uart) : uart_(uart) {}
+
+void UARTForwardComponent::loop() {
+  while (uart_->available()) {
+    int c = uart_->read();
+    if (c != -1) {
+      // Send via HTTP or do whatever processing here
+      ESP_LOGD("uart_forward", "Got char: %c", (char)c);
+    }
+  }
+}
+
 
 
