1e2ef005abf7f3d6c49ffa0ada99998ab0a5a8b1
FancyOtter99 <pizza.great@protonmail.com>
2025-06-12 10:48:06 -0400

    Update uart_forward.cpp

--- components/uart_forward/uart_forward.cpp
+++ components/uart_forward/uart_forward.cpp
@@ -1,14 +1,13 @@
 #include "uart_forward.h"
+
 namespace esphome {
 namespace uart_forward {
 
-// No setup required, everything is in loop
-void register_uart_forward(UARTForwardComponent *comp) {
-  App.register_component(comp);
-}
+// Nothing else needed here for now
 
 }  // namespace uart_forward
 }  // namespace esphome
+
 
 
 
