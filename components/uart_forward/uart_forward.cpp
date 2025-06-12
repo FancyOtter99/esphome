8f2ef5609131a159ce60fdc069126c16b9c81048
FancyOtter99 <pizza.great@protonmail.com>
2025-06-12 10:26:57 -0400

    Update uart_forward.cpp

--- components/uart_forward/uart_forward.cpp
+++ components/uart_forward/uart_forward.cpp
@@ -1,13 +1,5 @@
 #include "uart_forward.h"
 
-void UARTForwardComponent::loop() {
-  if (uart_->available()) {
-    std::string data;
-    while (uart_->available()) {
-      char c = uart_->read();
-      data.push_back(c);
-    }
-    text_sensor_->publish_state(data.c_str());
-  }
-}
+// Nothing needed here since everything is inline in .h except includes
+
 
