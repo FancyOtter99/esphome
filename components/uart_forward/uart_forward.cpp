02a17102d264b446e85febe461285599fa460026
FancyOtter99 <pizza.great@protonmail.com>
2025-06-12 10:43:39 -0400

    Update uart_forward.cpp

--- components/uart_forward/uart_forward.cpp
+++ components/uart_forward/uart_forward.cpp
@@ -1,31 +1,15 @@
 #include "uart_forward.h"
-#include "esphome/core/log.h"
-#include "esphome/components/http_request/http_request_component.h"
-
 namespace esphome {
 namespace uart_forward {
 
-static const char *const TAG = "uart_forward";
-
-void UARTForwardComponent::loop() {
-  while (available()) {
-    char c = read();
-    buffer_ += c;
-
-    // Optional: terminate on newline and send it
-    if (c == '\n') {
-      ESP_LOGI(TAG, "Received UART data: %s", buffer_.c_str());
-
-      // Send HTTP request (requires http_request integration setup in YAML)
-      // id(my_http).post("https://your.server", buffer_);
-
-      buffer_.clear();
-    }
-  }
+// No setup required, everything is in loop
+void register_uart_forward(UARTForwardComponent *comp) {
+  App.register_component(comp);
 }
 
 }  // namespace uart_forward
 }  // namespace esphome
+
 
 
 
