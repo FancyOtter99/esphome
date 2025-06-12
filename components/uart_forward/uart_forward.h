52674103b685c6dad34978547973a76839bda396
FancyOtter99 <pizza.great@protonmail.com>
2025-06-12 10:35:51 -0400

    Update uart_forward.h

--- components/uart_forward/uart_forward.h
+++ components/uart_forward/uart_forward.h
@@ -1,12 +1,20 @@
-// uart_forward.h
 #pragma once
-#include "esphome.h"
 
-class UARTForwardComponent : public esphome::Component {
+#include "esphome/core/component.h"
+#include "esphome/components/uart/uart.h"
+
+namespace esphome {
+namespace uart_forward {
+
+class UARTForwardComponent : public Component, public uart::UARTDevice {
  public:
-  explicit UARTForwardComponent(esphome::UARTComponent *uart);
+  explicit UARTForwardComponent(uart::UARTComponent *parent) : uart::UARTDevice(parent) {}
+
   void loop() override;
 
  protected:
-  esphome::UARTComponent *uart_;
+  std::string buffer_;
 };
+
+}  // namespace uart_forward
+}  // namespace esphome
